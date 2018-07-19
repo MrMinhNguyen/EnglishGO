ce81bab07df8d1c72db92686b77d7e3e07c6b6f6
Edward Newgate <badaodaica1991@gmail.com>
2018-07-19 12:01:23 +0700

    19-7

---
+++ app/src/main/java/sepm/englishgo/MainActivity.java
@@ -0,0 +1,39 @@
+package sepm.englishgo;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    DocumentReference docRef = FirebaseFirestore.getInstance().collection("word").document("W1");
+     
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        HashMap<String, Object> newWord = new HashMap<>();
+        newWord.put("content","Abc");
+
+        docRef.set(newWord).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d("Success","success");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("Success","Fail");
+            }
+        });
+    }
+}
