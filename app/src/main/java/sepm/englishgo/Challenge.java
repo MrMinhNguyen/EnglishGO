720498953ed9ad777cb076a185c87750ccd13a49
Edward Newgate <badaodaica1991@gmail.com>
2018-07-26 11:05:20 +0700

    26/7

--- app/src/main/java/sepm/englishgo/Challenge.java
+++ app/src/main/java/sepm/englishgo/Challenge.java
@@ -3,11 +3,14 @@ package sepm.englishgo;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -16,11 +19,16 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Random;
 
 public class Challenge extends AppCompatActivity {
 
     public static Word VOCABULARY;
+    TextToSpeech t1;
+    private boolean correctPhoto;
+    private boolean correctPron;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +62,14 @@ public class Challenge extends AppCompatActivity {
                     }
                 });
 
+
+    }
+
+    public void check(View v){
+        if (this.correctPhoto && this.correctPron){
+            Intent changeView = new Intent( Challenge.this, Result.class);
+            startActivity(changeView);
+        }
     }
 
     public void getBack(View view){
@@ -69,8 +85,29 @@ public class Challenge extends AppCompatActivity {
                 .setTitle("Hint")
                 .create();
 
-
         hint.show();
+    }
+
+    public void textToSpeech(View view){
+        try {
+            t1 = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+                @Override
+                public void onInit(int status) {
+                    if(status != TextToSpeech.ERROR) {
+                        t1.setLanguage(Locale.UK);
+                        t1.speak(VOCABULARY.getContent(), TextToSpeech.QUEUE_FLUSH, null);
+                    }
+                }
+            });
+
+            this.correctPron = true;
+            this.correctPhoto = true;
+
+            check(view);
+        }
+        catch (NullPointerException e){
+            System.out.println("No word error");
+        }
     }
 
 }
--- app/src/main/java/sepm/englishgo/Result.java
+++ app/src/main/java/sepm/englishgo/Result.java
@@ -1,9 +1,17 @@
 package sepm.englishgo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class Result extends AppCompatActivity {
+
+    TextToSpeech t1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +19,41 @@ public class Result extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.result);
 
+        TextView word = findViewById(R.id.resultWord);
+        TextView explanation = findViewById(R.id.explanation);
+        TextView sampleSen = findViewById(R.id.sampleSen);
+
+        word.setText((Challenge.VOCABULARY.getContent()));
+        explanation.setText("Explanation:\n"+Challenge.VOCABULARY.getExplanation());
+        sampleSen.setText("Sample Sentence:\n"+Challenge.VOCABULARY.getSampleSen());
+
+
+    }
+
+    public void newChallenge(View v){
+        Intent changeView = new Intent( Result.this, Challenge.class);
+        startActivity(changeView);
+    }
+
+    public void textToSpeech(View view){
+        try {
+            t1 = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+                @Override
+                public void onInit(int status) {
+                    if(status != TextToSpeech.ERROR) {
+                        t1.setLanguage(Locale.UK);
+                        t1.speak(Challenge.VOCABULARY.getContent(), TextToSpeech.QUEUE_FLUSH, null);
+                    }
+                }
+            });
+        }
+        catch (NullPointerException e){
+            System.out.println("No word error");
+        }
+    }
+
+    public void getBack(View view){
+        Intent changeView = new Intent( Result.this, ChooseTopic.class);
+        startActivity(changeView);
     }
 }
